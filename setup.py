eba8851d70693f2b2da96d16339fc42b1eb605ad
AnandBhagat345 <anandbhagat345@.com>
2026-04-18 13:42:45 +0530

    my first commit

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages,setup 
+from typing import List
+
+
+
+
+def get_requirements(file_path:str)->List[str]:
+    requiments=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+
+        if "-e ." in requirements:
+            requirements.remove("-e .")
+            
+    return requirements
+
+
+setup(
+    name = 'Fault-detection',
+    version='0.0.1',
+    author='anand',
+    author_email='anandbhagat345@gmai.com',
+    install_requires=get_requirements('requirements.txt'),
+    packages=find_packages()
+)
